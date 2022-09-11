2382645931a1375f9f199d3d726ac37c52b103f6
ZZELAV <valentino.panico@icloud.com>
2022-09-11 21:50:39 +0200

    favicon fix

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/lernportfolio-ZZELAV/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'data/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
