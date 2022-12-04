b1826f3e8351a82b730b8e7a6039bc8dcd873593
ZZELAV <valentino.panico@icloud.com>
2022-12-04 15:15:50 +0100

    v1.14

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -157,7 +157,7 @@ const config = {
             ],
           },
         ],
-        copyright: `v1.12<br>Copyright © ${new Date().getFullYear()} Lernjournal, Valentino Panico. Built with Docusaurus.`,
+        copyright: `v1.14<br>Copyright © ${new Date().getFullYear()} Lernjournal, Valentino Panico. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
